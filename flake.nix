36b6b96409c4734759626193ae207c1194663b45
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-30 13:45:01 +0100

    chore: init flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachDefaultSystem =
+        f:
+        lib.systems.flakeExposed
+        |> map (s: lib.mapAttrs (_: v: { ${s} = v; }) (f s))
+        |> lib.foldAttrs lib.mergeAttrs { };
+    in
+    eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            buildInputs = with pkgs; [ hugo ];
+          };
+        };
+
+        formatter = pkgs.nixfmt;
+      }
+    );
+}
